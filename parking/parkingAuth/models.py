44a419b1a92c5f45f009eecf6480317f1d03470e
Permyakov Roman <r.permyakov@skbkontur.ru>
2022-11-05 13:43:24 +0600

    Сервис регистрации и авторизации
    branch: 7108080_AuthRests
    https://mind.kaiten.ru/space/91109/card/7108080

---
+++ parking/parkingAuth/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        max_length=100, unique=True, blank=True, null=True, validators=[username_validator])
+    email = models.EmailField(unique=True, blank=True, null=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    phone = models.CharField(unique=True, max_length=12)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone'
+    EMAIL_FIELD = 'email'
