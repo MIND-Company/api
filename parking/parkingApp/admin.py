14b42e8ff467370c901b919851c6c9ce4eb17b70
Permyakov Roman <r.permyakov@skbkontur.ru>
2022-11-05 16:07:16 +0600

    настроил отображение в админке
    branch: 7417433-GetParkingOwnerRests
    https://mind.kaiten.ru/space/91109/card/7417433

--- parking/parkingApp/admin.py
+++ parking/parkingApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Park, ParkingInfo, Car
 
-# Register your models here.
+admin.site.register(Park)
+admin.site.register(ParkingInfo)
+admin.site.register(Car)
--- parking/parkingApp/models.py
+++ parking/parkingApp/models.py
@@ -10,6 +10,9 @@ class Car(models.Model):
     color = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return self.number
+
 
 class Park(models.Model):
     description = models.CharField(max_length=128)
@@ -21,11 +24,17 @@ class Park(models.Model):
     webAddress = models.CharField(max_length=128)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return self.description
+
 
 class ParkingInfo(models.Model):
     entry_time = models.DateTimeField(auto_now_add=True)
-    checkout_time = models.DateTimeField(auto_now_add=False, null=True)
+    checkout_time = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     calculated_price = models.DecimalField(
-        max_digits=9, decimal_places=2, validators=[MinValueValidator(0)], null=True)
+        max_digits=9, decimal_places=2, validators=[MinValueValidator(0)], null=True, blank=True)
     park = models.ForeignKey(Park, on_delete=models.SET_NULL, null=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'park: {self.park}, car: {self.car}'
