187719bbe8c7980a03f8aabf9071a4ff82f457dc
Permyakov Roman <r.permyakov@skbkontur.ru>
2022-12-02 15:19:50 +0500

    добавил поля широты и долготы для парковки
    branch: main
    https://mind.kaiten.ru/space/91109/card/

--- parking/parkingApp/models.py
+++ parking/parkingApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from parkingAuth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 
 
 class Car(models.Model):
@@ -15,13 +15,16 @@ class Car(models.Model):
 
 
 class Park(models.Model):
-    description = models.CharField(max_length=128)
+    description = models.CharField(
+        max_length=128, blank=False, default=None, validators=[MinLengthValidator(1)])
     place_count = models.IntegerField(validators=[
         MinValueValidator(
             1, "Количество мест не может быть отрицательным и меньше 1"),
         MaxValueValidator(10_001, "Таких больших парковок не бывает")])
-    address = models.CharField(max_length=512)
-    webAddress = models.CharField(max_length=128)
+    address = models.CharField(max_length=512, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    web_address = models.CharField(max_length=128, null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
@@ -30,7 +33,8 @@ class Park(models.Model):
 
 class ParkingInfo(models.Model):
     entry_time = models.DateTimeField(auto_now_add=True)
-    checkout_time = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    checkout_time = models.DateTimeField(
+        auto_now_add=False, null=True, blank=True)
     calculated_price = models.DecimalField(
         max_digits=9, decimal_places=2, validators=[MinValueValidator(0)], null=True, blank=True)
     park = models.ForeignKey(Park, on_delete=models.SET_NULL, null=True)
