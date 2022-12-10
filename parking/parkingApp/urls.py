a30dbdebb180da19a697539a3f06e04421f6495a
Permyakov Roman <r.permyakov@skbkontur.ru>
2022-12-10 17:33:25 +0500

    добавил круды для машины
    branch: 8205547-CRUDForCar
    https://mind.kaiten.ru/space/91109/card/8205547

--- parking/parkingApp/urls.py
+++ parking/parkingApp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ParkViewSet, ParkingInfoViewSet, ParkingRecord, ParkCreate
+from .views import ParkViewSet, ParkingInfoViewSet, ParkingRecord, ParkCreate, CarViewSet
 
 
 router = DefaultRouter()
 router.register(r'parks', ParkViewSet, basename="parks")
 router.register('parkings', ParkingInfoViewSet, basename="parking-infos")
+router.register(r'cars', CarViewSet, basename="cars")
 
 urlpatterns = [    
     path('parkings/create/', ParkingRecord.as_view()),
