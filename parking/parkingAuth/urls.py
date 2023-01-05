c234625e82432ead3ed964fc999c1d02c645ca24
Permyakov Roman <r.permyakov@skbkontur.ru>
2023-01-05 16:19:49 +0500

    добавил корявенький ендпоинт для профиля
    branch: 8471999-UserProfileRest
    https://mind.kaiten.ru/space/91109/card/8471999

--- parking/parkingAuth/urls.py
+++ parking/parkingAuth/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', UserViewSet.as_view(), name='profile_edit'),
+    path('', include(router.urls)),
 ]
--- parking/parkingAuth/views.py
+++ parking/parkingAuth/views.py
@@ -1,8 +1,37 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, views, status
+from rest_framework.response import Response
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+# TODO: Это, конечно капец, надо разобраться как это делается по-нормальному
+class UserViewSet(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
+
+    def patch(self, request):
+        user = request.user
+        data = request.data
+        if data.get('email'):
+            user.email = data['email']
+        if data.get('first_name'):
+            user.first_name = data['first_name']
+        if data.get('last_name'):
+            user.last_name = data['last_name']
+        if data.get('username'):
+            user.username = data['username']
+
+        user_data = UserSerializer(data=vars(user))
+        if user_data.is_valid():
+            user.save()
+        else:
+            return Response(user_data.errors, status.HTTP_400_BAD_REQUEST)
+
+        return Response(user_data.data, status=status.HTTP_200_OK)
