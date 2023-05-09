9547f50dfb655f053c3fadaee969fe051c55c76b
Permyakov Roman <r.permyakov@skbkontur.ru>
2023-05-09 13:52:12 +0500

    add ConfirmationCode to admin
    branch: main
    https://mind.kaiten.ru/space/91109/card/

--- parking/parkingApp/admin.py
+++ parking/parkingApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Park, ParkingInfo, Car, Price
+from .models import Park, ParkingInfo, Car, Price, ConfirmationCode
 
 admin.site.register(Park)
 admin.site.register(ParkingInfo)
 admin.site.register(Car)
 admin.site.register(Price)
+admin.site.register(ConfirmationCode)
